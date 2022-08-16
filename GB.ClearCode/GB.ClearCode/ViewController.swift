1beb6f46b32bd607732e22bd17b1bbde7d810101
Cayenne007 <46312967+Cayenne007@users.noreply.github.com>
2022-08-16 17:54:44 +0300

    init

---
+++ GB.ClearCode/GB.ClearCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GB.ClearCode
+//
+//  Created by Cayenne on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
